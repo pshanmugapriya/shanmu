355d4eac3f00fdd8757fa6063161e04ffb61f421
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-07-12 13:38:26 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,21 @@
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n;
+System.out.println("enter the number:");
+Scanner scan=new Scanner(System.in);
+n=scan.nextInt();
+if(n%2==0){
+	System.out.println("even");
+	
+}
+else if(n%2==1){
+	System.out.println("odd");
+	
+}
+else
+	System.out.println("INVALID");
+	}
+
+}
