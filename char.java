78a61d9ed2b4a05177f98f74c2e04e5782b86719
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-07-13 09:57:08 +0530

    Update char.java

--- char.java
+++ char.java
@@ -2,13 +2,13 @@ public class Char {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-char c;
+char ch;
 Scanner scan=new Scanner(System.in);
 c=scan.next().charAt(0);
-if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
 	System.out.println("vowel");
 }
-else if(c>='a'&&c<='z'&& (!(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'))){
+else if(ch>='b'&&ch<='z'&& (!(ch=='e'||ch=='i'||ch=='o'||ch=='u'))){
 	System.out.println("consonant");
 }
 else{
