bb479da8409aff77411eb82ff675dbff1f662cba
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-07-14 10:24:09 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+public class shanmu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("enter the number");
+Scanner scan=new Scanner(System.in);
+int number;
+number=scan.nextInt();
+for(int i=0;i<number;i++){
+	System.out.println("hello");
+}
+	}
+
+}
