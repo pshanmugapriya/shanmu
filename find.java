30c6351245e95146bd802ff38f05b932045bfd98
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-07-14 15:07:36 +0530

    Create find.java

---
+++ find.java
@@ -0,0 +1,14 @@
+public class Sghj {
+
+
+public static void main(String[] args) {
+	System.out.println("enter the number:");
+	Scanner scan=new Scanner(System.in);
+	int n,k;
+	n=scan.nextInt();
+	k=Integer.toString(n).length();
+	System.out.println(k);
+}
+}
+
+	
