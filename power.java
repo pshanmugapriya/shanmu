e6a2d6c11d0a6fd49119528cb464849a74b86669
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-07-14 15:30:48 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,14 @@
+public class Sghj {
+
+
+public static void main(String[] args) {
+	System.out.println("enter the number:");
+	Scanner scan=new Scanner(System.in);
+	int n,k,pow;
+n=scan.nextInt();
+k=scan.nextInt();
+int c=(int) Math.pow(n,k);
+System.out.println(c);
+}
+
+}	
