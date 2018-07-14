7ddf281aa329e9afdab4ce1f394258e1653ec900
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-07-14 13:57:33 +0530

    Update char.java

--- char.java
+++ char.java
@@ -5,10 +5,10 @@ public class Char {
 char ch;
 Scanner scan=new Scanner(System.in);
 c=scan.next().charAt(0);
-if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+if(ch<1 && ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
 	System.out.println("vowel");
 }
-else if(ch>='b'&& ch<='z'){
+else if(ch<1 && ch>='b'&& ch<='z'){
 	System.out.println("consonant");
 }
 else{
