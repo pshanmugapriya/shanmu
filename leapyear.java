0b4ceed10802e53cacc6a4dae5dc2bb8c3376e03
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-07-14 10:12:04 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,18 @@
+public class Feyis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("enter the number");
+Scanner scan=new Scanner(System.in);
+int number;
+number=scan.nextInt();
+if(number%4==0){
+	System.out.println("leap year");
+	
+}
+else{
+	System.out.println("not leap year");
+}
+	}
+
+}
