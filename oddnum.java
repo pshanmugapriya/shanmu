b8972ac557b09b6d093924d57e56aefd5dd6a6f4
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-07-16 16:04:54 +0530

    Create oddnum.java

---
+++ oddnum.java
@@ -0,0 +1,19 @@
+public class Oddnum{
+
+	public static void main(String[] args) {
+		System.out.println("enter the number:");
+		Scanner scan=new Scanner (System.in);
+		int n,i=0,q;
+		n=scan.nextInt();
+        q=scan.nextInt();
+        if(n<100000 && q<100000){
+        	for(i=n+1;i<q;i++){
+        		if(i%2==1)
+        		System.out.println(i);
+        	}
+        }
+
+		}
+
+}
+
