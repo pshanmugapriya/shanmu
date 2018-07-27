def71b4b6894ddcc455ef8729b1ed4c50872eb8f
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-07-27 17:13:50 +0530

    Create isomorphic.java

---
+++ isomorphic.java
@@ -0,0 +1,39 @@
+public class Isomorphic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("enter the String");
+Scanner in=new Scanner(System.in);
+String s=in.nextLine();
+String []str=s.split(" ");
+int l=str.length;
+int i,count=0,flag=0,j,count1=0,flag1=0;
+char ch[]=str[0].toCharArray();
+char c[]=str[1].toCharArray();
+if(ch.length==c.length){
+   for(i=0;i<ch.length-1;i++){
+	   if(ch[i]==ch[i+1]){
+		   count++;
+	 }
+	   else{
+		   flag=0;
+		   
+	   }}
+	   for(j=0;j<c.length-1;j++){
+		   if(c[j]==c[j+1]){
+			   count1++;
+			 
+		   }else{
+			   flag1=0;
+			   } } }
+   if(count==count1){
+	   System.out.println("yes");
+	   
+   }else{
+	   
+	   System.out.println("no");
+	   
+}
+	}
+
+}
