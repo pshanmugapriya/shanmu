c69d08de92d8d7a0fab8e8f6f0777aa2a2685cce
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-07-28 17:14:08 +0530

    Create camelcase.java

---
+++ camelcase.java
@@ -0,0 +1,9 @@
+public class CamelCase {
+public static void main(String[] args) {
+System.out.println("enter the string");
+Scanner in=new Scanner(System.in);
+String s=in.nextLine();
+String[] st =s.split(" ");
+for(int i=0;i<st.length;i++){
+	System.out.println((st[i].substring(0, 1)).toUpperCase()+st[i].substring(1));
+}}}
