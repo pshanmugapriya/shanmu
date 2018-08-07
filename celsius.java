073a892448cb4363cd4b04ff546d108d0c9de414
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-08-07 12:03:13 +0530

    Create celsius.java

---
+++ celsius.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Kelvin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("enter the celsius");
+Scanner in=new Scanner(System.in);
+int c=in.nextInt();
+int k=c+273;
+System.out.println(k);
+	}}
