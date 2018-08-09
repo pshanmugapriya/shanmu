85649af1e5a595a58de87316e5819feadb619447
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-08-09 14:45:03 +0530

    Create equa.java

---
+++ equa.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Equa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("enter the number");
+Scanner in=new Scanner(System.in);
+int a=in.nextInt();
+int b=in.nextInt();
+int c=in.nextInt();
+int e=(a*b)/c;
+System.out.println(e);
+	}
+
+}
