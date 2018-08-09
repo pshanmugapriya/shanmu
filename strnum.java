9c8bd1d0549dd81f54408fc4ccdf75494be95eb3
pshanmugapriya <40538353+pshanmugapriya@users.noreply.github.com>
2018-08-09 12:31:29 +0530

    Create strnum.java

---
+++ strnum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Strnum {
+
+	public static void main(String[] args) {
+		System.out.println("enter the string");
+		Scanner in=new Scanner(System.in);
+		String s=in.next();
+		System.out.println("enter the number");
+		int num=in.nextInt();
+	for(int i=0;i<s.length();i++){
+		char[] si=s.toCharArray();
+	}
+	System.out.println(s.substring(s.length()-num));
+	
+
+	}
+
+}
